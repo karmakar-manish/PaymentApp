79cef9f975d3d67eccf3ca2982a68e6a2b6c803a
karmakar-manish <manishkarmakar106@gmail.com>
2025-05-06 00:49:17 +0530

    Added cors origin of netlify

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,10 +5,22 @@ import cookieParser from "cookie-parser";
 const app = express()
 
 app.use(express.json()) //body parser
+const allowedOrigins = [
+    "http://localhost:5173", // dev
+    "https://paymentappmanish.netlify.app", // replace with your real Netlify domain
+  ];
+  
 app.use(cors({
-    origin: "http://localhost:5173",    //frontend URL
-    credentials: true
-}))
+origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+    callback(null, true);
+    } else {
+    callback(new Error("Not allowed by CORS"));
+    }
+},
+credentials: true  // if using cookies/session
+}));
+
 app.use(cookieParser())
 //route requests that start with api/v1
 app.use("/api/v1",mainRoute)
