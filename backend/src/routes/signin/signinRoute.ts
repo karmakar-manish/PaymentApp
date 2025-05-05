b52c743f1c07317d8fb314d3cd7bec702b375636
karmakar-manish <manishkarmakar106@gmail.com>
2025-05-06 01:00:41 +0530

    Added cors origin of netlify

--- backend/src/routes/signin/signinRoute.ts
+++ backend/src/routes/signin/signinRoute.ts
@@ -75,7 +75,7 @@ router.post("/phonePassword", async(req: any,res: any)=>{
     res.cookie("token", token, {
         httpOnly: true, // Prevents JavaScript from accessing the cookie
         sameSite: "none",    //cross-site post/put/delete/fetch not allowed from different origin
-        secure: false,
+        secure: true,
         maxAge: 24*60*60*1000,   //1 day
         // path: "/"
     })
@@ -126,7 +126,7 @@ router.post("/providerLogin", async(req:any, res:any)=>{
             res.cookie("token", token, {
                 httpOnly: true, // Prevents JavaScript from accessing the cookie
                 sameSite: "none",    //cross-site post/put/delete/fetch not allowed from different origin
-                secure: false,
+                secure: true,
                 maxAge: 24*60*60*1000,   //1 day
                 // path: "/"
             })
--- backend/src/routes/signup/signupRoute.ts
+++ backend/src/routes/signup/signupRoute.ts
@@ -51,7 +51,7 @@ router.post("/phonePassword", async(req: any, res:any)=>{
        //create cookie
         res.cookie("token", token, {
             httpOnly: true, // Prevents JavaScript from accessing the cookie
-            sameSite: "lax",    //cross-site post/put/delete/fetch not allowed from different origin
+            sameSite: "none",    //cross-site post/put/delete/fetch not allowed from different origin
             secure: true,
             maxAge: 24*60*60*1000,   //1 day
             // path: "/"
