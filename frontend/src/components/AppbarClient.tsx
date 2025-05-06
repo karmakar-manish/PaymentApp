aa0971531637d8596709080d53d5a151daf7bf55
karmakar-manish <manishkarmakar106@gmail.com>
2025-05-06 20:49:07 +0530

    Changed frontend page

--- frontend/src/components/AppbarClient.tsx
+++ frontend/src/components/AppbarClient.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import Appbar from "./AppbarComponent";
-import { useState } from "react";
 import axios from "axios";
 import { BACKEND_URL } from "../config";
 
