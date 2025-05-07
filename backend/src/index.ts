254890a6db0d0d3bcac520407b8f15175b99a8b9
karmakar-manish <manishkarmakar106@gmail.com>
2025-05-07 15:38:26 +0530

    Fixed cors error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,9 +7,11 @@ import {cors} from "hono/cors"
 const app = new Hono()
 
 app.use("/*", cors({
-    origin: "http://localhost:5173",    //frontend url
+    // origin: "http://localhost:5173",    //frontend url
+    origin: "https://paymentappmanish.netlify.app",
     credentials: true
 }))
+
 
 app.route("/api/v1", mainRoute)
 
